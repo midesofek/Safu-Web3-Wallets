e4d449bfc39334a4be2351337e7737b6efb1798a
midesofek <midesofek@gmail.com>
2025-10-19 23:09:33 +0100

    set up server using fastify

---
+++ server.js
@@ -0,0 +1,20 @@
+const Fastify = require("fastify");
+const dotenv = require("dotenv").config();
+require("./utils/database");
+const walletRoutes = require("./routes/wallet");
+
+const app = Fastify({ logger: true });
+
+app.register(walletRoutes);
+
+const start = async () => {
+  try {
+    await app.listen({ port: process.env.PORT });
+    console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
