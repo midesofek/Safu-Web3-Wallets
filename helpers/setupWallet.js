1f80921d1879d0801c3c17b86990be13348fd1fb
midesofek <midesofek@gmail.com>
2025-10-20 04:48:17 +0100

    added verification layer to createWallet and importWallet functions then added new 'recoverWallet' to enable users recover their seedphrase by entering their password

--- helpers/setupWallet.js
+++ helpers/setupWallet.js
@@ -1,33 +1,37 @@
 const { ethers } = require("ethers");
 const bip39 = require("bip39");
-const { encryptData } = require("../utils/crypto");
+const { encryptWithPassword, decryptWithPassword } = require("../utils/crypto");
 const UserWallet = require("../models/UserWallet");
+const { verifyUserPassword } = require("./authHelper");
 
-const createWallet = async () => {
-  const mnemonic = bip39.generateMnemonic(128); // 12 words
+const createWallet = async (userId, password) => {
+  await verifyUserPassword(userId, password);
+
+  const mnemonic = bip39.generateMnemonic(128);
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
 
-  const encryptedSeed = encryptData(mnemonic);
+  const encryptedSeed = encryptWithPassword(mnemonic, password);
 
   const newWallet = await UserWallet.create({
+    userId,
     address: wallet.address,
     encryptedSeed,
   });
 
-  return { address: newWallet.address, mnemonic }; // Implement Logic to Only return mnemonic once
+  return { address: newWallet.address, mnemonic };
 };
 
-const importWallet = async (mnemonic) => {
-  if (!bip39.validateMnemonic(mnemonic)) {
-    throw new Error("Invalid mnemonic");
-  }
+const importWallet = async (userId, password, mnemonic) => {
+  await verifyUserPassword(userId, password);
+  if (!bip39.validateMnemonic(mnemonic)) throw new Error("Invalid mnemonic");
 
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-  const encryptedSeed = encryptData(mnemonic);
+  const encryptedSeed = encryptWithPassword(mnemonic, password);
 
   let existing = await UserWallet.findOne({ address: wallet.address });
   if (!existing) {
     existing = await UserWallet.create({
+      userId,
       address: wallet.address,
       encryptedSeed,
     });
@@ -36,4 +40,49 @@ const importWallet = async (mnemonic) => {
   return { address: wallet.address };
 };
 
-module.exports = { createWallet, importWallet };
+const recoverWallet = async (userId, password) => {
+  await verifyUserPassword(userId, password);
+
+  const wallet = await UserWallet.findOne({ userId });
+  if (!wallet) throw new Error("No wallet found for this user");
+
+  const mnemonic = decryptWithPassword(wallet.encryptedSeed, password);
+  if (!mnemonic) throw new Error("Failed to decrypt. Wrong password?");
+
+  return { mnemonic };
+};
+
+// const createWallet = async () => {
+//   const mnemonic = bip39.generateMnemonic(128); // 12 words; 256 for `24` words
+//   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+
+//   const encryptedSeed = encryptData(mnemonic);
+
+//   const newWallet = await UserWallet.create({
+//     address: wallet.address,
+//     encryptedSeed,
+//   });
+
+//   return { address: newWallet.address, mnemonic };
+// };
+
+// const importWallet = async (mnemonic) => {
+//   if (!bip39.validateMnemonic(mnemonic)) {
+//     throw new Error("Invalid mnemonic");
+//   }
+
+//   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+//   const encryptedSeed = encryptData(mnemonic);
+
+//   let existing = await UserWallet.findOne({ address: wallet.address });
+//   if (!existing) {
+//     existing = await UserWallet.create({
+//       address: wallet.address,
+//       encryptedSeed,
+//     });
+//   }
+
+//   return { address: wallet.address };
+// };
+
+module.exports = { createWallet, importWallet, recoverWallet };
