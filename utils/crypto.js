bdd6882fa845f2a94243c9a680df328883a04089
midesofek <midesofek@gmail.com>
2025-10-20 04:40:26 +0100

    updated encryption utils to support encryption using PBKDF2

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,14 +1,50 @@
 const CryptoJS = require("crypto-js");
+const bcrypt = require("bcryptjs");
 
-const SECRET = process.env.ENCRYPTION_KEY;
+// const SECRET = process.env.ENCRYPTION_KEY;
+const PBK_WALLET_SALT = process.env.PBK_WALLET_SALT;
 
-const encryptData = (text) => {
-  return CryptoJS.AES.encrypt(text, SECRET).toString();
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
 };
 
-const decryptData = (cipher) => {
-  const bytes = CryptoJS.AES.decrypt(cipher, SECRET);
+const verifyPassword = async (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+/// @dev fn derives a cryptographic key from the user's password using PBKDF2.
+const deriveKey = (password) => {
+  return CryptoJS.PBKDF2(password, PBK_WALLET_SALT, {
+    keySize: 256 / 32,
+    iterations: 1000,
+  }).toString();
+};
+
+// const encryptData = (text) => {
+//   return CryptoJS.AES.encrypt(text, SECRET).toString();
+// };
+
+// const decryptData = (cipher) => {
+//   const bytes = CryptoJS.AES.decrypt(cipher, SECRET);
+//   return bytes.toString(CryptoJS.enc.Utf8);
+// };
+
+const encryptWithPassword = (text, password) => {
+  const key = deriveKey(password);
+  return CryptoJS.AES.encrypt(text, key).toString();
+};
+
+const decryptWithPassword = (cipher, password) => {
+  const key = deriveKey(password);
+  const bytes = CryptoJS.AES.decrypt(cipher, key);
   return bytes.toString(CryptoJS.enc.Utf8);
 };
 
-module.exports = { encryptData, decryptData };
+module.exports = {
+  hashPassword,
+  verifyPassword,
+  deriveKey,
+  encryptWithPassword,
+  decryptWithPassword,
+};
